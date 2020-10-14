62a84492cf3b9302b9f6fa599a2a1686b2a03775
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-10-13 21:59:11 -0500

    [Updated]Added the movement to the player and the username appears in the chat

--- Backend/main.js
+++ Backend/main.js
@@ -3,23 +3,12 @@ const cors = require("cors");
 const { response } = require("express");
 const http = require("http").createServer(express);
 const io = require("socket.io")(http);
-// const { MongoClient } = require("mongodb");
-
-// const pass = process.env.MongoDB_password;
-
-// const client = new MongoClient("mongodb+srv://MiguelCF06:" + pass + "@ziragame.qfibu.mongodb.net/gamedev?retryWrites=true&w=majority");
 
 express.use(cors());
-
-var collection;
 
 io.on("connection", (socket) => {
     socket.on("join", async (gameId) => {
         try {
-            // let result = await collection.findOne({ "_id": gameId });
-            // if (!result) {
-            //     await collection.insertOne({ "_id": gameId, messages: [] });
-            // }
             socket.join(gameId);
             socket.emit("joined", gameId);
             socket.activeRoom = gameId;
@@ -36,29 +25,12 @@ io.on("connection", (socket) => {
     // });
 
     socket.on("message", (message) => {
-        // collection.updateOne({ "_id": socket.activeRoom }, {
-        //     "$push": {
-        //         "messages": message
-        //     }
-        // });
         io.to(socket.activeRoom).emit("message", message);
     });
 });
 
-// // Endpoint
-// express.get("/chats", async (request, response) => {
-//     // try {
-//     //     let result = await collection.findOne({ "_id": request.query.room });
-//     //     response.send(result);
-//     // } catch (err) {
-//     //     response.status(500).send({ message: err.message });
-//     // }
-// });
-
 http.listen(3000, async () => {
     try {
-        // await client.connect();
-        // collection = client.db("gamedev").collection("chats");
         console.log("Listening on port :%s", http.address().port);
     } catch (err) {
         console.error(err);
